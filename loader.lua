4a7e9c251e3b443455a8b85db9510f715feb54bc
pastelrbx <itspastelxx@gmail.com>
2025-12-12 22:06:10 +0100

    refactor codebase

--- loader.lua
+++ loader.lua
@@ -11,7 +11,7 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/Divulgate/BDTestCheat/'..readfile('BDTestCheat/profiles/commit.txt')..'/'..select(1, path:gsub('BDTestCheat/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
@@ -34,7 +34,7 @@ local function wipeFolder(path)
 	end
 end
 
-for _, folder in {'newvape', 'newvape/games', 'newvape/profiles', 'newvape/assets', 'newvape/libraries', 'newvape/guis'} do
+for _, folder in {'newvape', 'BDTestCheat/games', 'BDTestCheat/profiles', 'BDTestCheat/assets', 'BDTestCheat/libraries', 'BDTestCheat/guis'} do
 	if not isfolder(folder) then
 		makefolder(folder)
 	end
@@ -42,18 +42,18 @@ end
 
 if not shared.VapeDeveloper then
 	local _, subbed = pcall(function() 
-		return game:HttpGet('https://github.com/7GrandDadPGN/VapeV4ForRoblox') 
+		return game:HttpGet('https://github.com/Divulgate/BDTestCheat') 
 	end)
 	local commit = subbed:find('currentOid')
 	commit = commit and subbed:sub(commit + 13, commit + 52) or nil
 	commit = commit and #commit == 40 and commit or 'main'
-	if commit == 'main' or (isfile('newvape/profiles/commit.txt') and readfile('newvape/profiles/commit.txt') or '') ~= commit then
+	if commit == 'main' or (isfile('BDTestCheat/profiles/commit.txt') and readfile('BDTestCheat/profiles/commit.txt') or '') ~= commit then
 		wipeFolder('newvape')
-		wipeFolder('newvape/games')
-		wipeFolder('newvape/guis')
-		wipeFolder('newvape/libraries')
+		wipeFolder('BDTestCheat/games')
+		wipeFolder('BDTestCheat/guis')
+		wipeFolder('BDTestCheat/libraries')
 	end
-	writefile('newvape/profiles/commit.txt', commit)
+	writefile('BDTestCheat/profiles/commit.txt', commit)
 end
 
-return loadstring(downloadFile('newvape/main.lua'), 'main')()
+return loadstring(downloadFile('BDTestCheat/main.lua'), 'main')()
